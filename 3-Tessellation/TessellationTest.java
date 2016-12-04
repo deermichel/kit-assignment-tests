8006b59495ece9f50007c5512b20c11f9b9af8ad
DeerMichel <mh74182@gmail.com>
2016-12-04 20:47:04 +0100

    Added more tests (Tessellation)

--- 3-Tessellation/TessellationTest.java
+++ 3-Tessellation/TessellationTest.java
@@ -40,9 +40,11 @@ public class TessellationTest {
 
     log("--- working toString method needed for following tests ---");
 
+    // copy
     Tile tile2 = tile1.copy();
     test(tile1.isExactlyEqualTo(tile2), "copy " + tile1.toString(), tile2.toString());
 
+    // rotating
     tile1.rotateClockwise();
     test(tile1.toString().equals("Y-YR-R"), "rotateClockwise -YR-RY = Y-YR-R", tile1.toString());
     tile1.rotateCounterClockwise();
@@ -50,9 +52,9 @@ public class TessellationTest {
     for (int i = 0; i < 6; i++) tile1.rotateClockwise();
     test(tile1.toString().equals("-YR-RY"), "6x rotateClockwise -YR-RY = -YR-RY", tile1.toString());
 
+    // comparing
     tile2 = new Tile();
     test(tile2.isEmpty(), "newTile().isEmpty()", "Returned false but should be true");
-
     test(tile1.isRotationEqualTo(tile1), "isRotationEqualTo reflexive test " + tile1.toString(), "Returned false but should be true");
     tile2 = tile1.copy();
     tile2.rotateClockwise();
@@ -60,6 +62,48 @@ public class TessellationTest {
     for (int i = 0; i < 4; i++) tile2.rotateCounterClockwise();
     test(tile2.isRotationEqualTo(tile1), tile2.toString() + " isRotationEqualTo " + tile1.toString(), "Returned false but should be true");
     test(!tile2.isExactlyEqualTo(tile1), tile2.toString() + " isExactlyEqualTo " + tile1.toString() + " is false", "Returned true");
+    test(tile1.canBeRecoloredTo(tile1), tile1.toString() + " canBeRecoloredTo " + tile1.toString(), "Returned false but should be true");
+    test(tile1.canBeRecoloredTo(tile2), tile1.toString() + " canBeRecoloredTo " + tile2.toString(), "Returned false but should be true");
+
+    LineType[] lines2 = { LineType.NONE, LineType.NONE, LineType.RED,
+                          LineType.NONE, LineType.RED,    LineType.NONE };
+    tile2 = new Tile(lines2);
+    test(!tile1.canBeRecoloredTo(tile2), tile1.toString() + " canBeRecoloredTo " + tile2.toString() + " is false", "Returned true");
+
+    test(tile1.dominates(tile2), tile1.toString() + " dominates " + tile2.toString(), "Returned false but should be true");
+    test(!tile2.dominates(tile1), tile2.toString() + " does not dominate " + tile1.toString(), "Returned true");
+    test(!tile1.dominates(tile1), "Tile cannot dominate itself", "Returned true");
+
+    test(tile1.hasSameColorsAs(tile1), tile1.toString() + " hasSameColorsAs " + tile1.toString(), "Returned false but should be true");
+    test(!tile2.hasSameColorsAs(tile1), tile2.toString() + " hasSameColorsAs " + tile1.toString() + " is false", "Returned true");
+
+    tile2 = tile1.copy();
+    tile2.rotateCounterClockwise();
+    tile1.rotateClockwise();
+    test(tile2.hasSameColorsAs(tile1), tile2.toString() + " hasSameColorsAs " + tile1.toString(), "Returned false but should be true");
+
+    // example interactions with tiles
+    log("--- example test of assignment (listing 2) ---");
+    tile1 = new Tile();
+    test(tile1.isEmpty(), tile1.toString() + " isEmpty", "Returned false but should be true");
+    test(tile1.getNumberOfColors() == 0, tile1.toString() + " getNumberOfColors == 0", "Returned " + Integer.toString(tile1.getNumberOfColors()));
+    test(tile1.toString().equals("------"), "toString of empty tile is ------", "Returned " + tile1.toString());
+    tile2 = new Tile(new LineType[] { LineType.NONE, LineType.YELLOW, LineType.NONE, LineType.NONE, LineType.NONE, LineType.YELLOW });
+    test(tile2.toString().equals("-Y---Y"), "toString of -Y---Y", "Returned " + tile1.toString());
+    test(!tile1.canBeRecoloredTo(tile2), tile1.toString() + " canBeRecoloredTo " + tile2.toString() + " is false", "Returned true");
+    Tile tile3 = new Tile(new LineType[] { LineType.NONE, LineType.YELLOW, LineType.RED, LineType.NONE, LineType.RED, LineType.YELLOW });
+    test(tile3.dominates(tile1), tile3.toString() + " dominates " + tile1.toString(), "Returned false but should be true");
+    test(tile3.dominates(tile2), tile3.toString() + " dominates " + tile2.toString(), "Returned false but should be true");
+    test(!tile2.canBeRecoloredTo(tile3), tile2.toString() + " canBeRecoloredTo " + tile3.toString() + " is false", "Returned true");
+    Tile tile4 = tile3.copy();
+    test(tile4.isExactlyEqualTo(tile3), tile4.toString() + " isExactlyEqualTo " + tile3.toString(), "Returned false but should be true");
+    for (int i = 0; i < 3; i++) tile4.rotateClockwise();
+    test(tile4.toString().equals("-RY-YR"), "Rotate " + tile3.toString() + " 3 times clockwise is -RY-YR", "Returned " + tile4.toString());
+    test(!tile4.isExactlyEqualTo(tile3), tile4.toString() + " isExactlyEqualTo " + tile3.toString() + " is false", "Returned true");
+    test(tile4.isRotationEqualTo(tile3), tile4.toString() + " isRotationEqualTo " + tile3.toString(), "Returned false but should be true");
+    test(tile4.canBeRecoloredTo(tile3), tile4.toString() + " canBeRecoloredTo " + tile3.toString(), "Returned false but should be true");
+
+
 
     log("--- more tests tba ---");
     //log("--- additional tests (validity not guaranteed!) ---");
