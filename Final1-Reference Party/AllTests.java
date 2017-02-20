bf6ea1c3aaf4e456931ceb053d9b5c9542db3d0a
DeerMichel <mh74182@gmail.com>
2017-02-21 00:11:55 +0100

    🍪 for test suites

---
+++ Final1-Reference Party/AllTests.java
@@ -0,0 +1,16 @@
+package edu.kit.informatik.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    StupidityTest.class,
+    C0Test.class,
+    C1Test.class,
+    C2Test.class,
+    C3Test.class
+})
+
+public class AllTests { }
