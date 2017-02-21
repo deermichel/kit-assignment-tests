997cffbc5ed48fa41c20e5e7cbeecf2bbfba1f24
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2017-02-21 14:47:46 +0100

    Add C5Test. BONUS: Some refactoring for common outputs in TestHelper.java

---
+++ Final1-Reference Party/TestHelper.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.hamcrest.Matchers.*;
+import org.junit.*;
+
+import edu.kit.informatik.Terminal;
+
+/**
+ * Test Helper for the Terminal Test Class Based on Common Outputs
+ *
+ * @author Florian Pfisterer
+ * Based on templates & inspiration from Micha Hanselmann
+ */
+
+public class TestHelper {
+
+    public static void expectOk(final String input) {
+        Terminal.addSingleLineOutputThatIsExactly(input, "Ok");
+    }
+
+    public static void expectError(final String input) {
+        Terminal.addSingleLineOutputThatMatches(input, startsWith("Error, "));
+    }
+}
