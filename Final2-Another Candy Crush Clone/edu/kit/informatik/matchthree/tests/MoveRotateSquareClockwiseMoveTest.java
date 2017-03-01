df363ddccb339476342c0ba6db5695a93644aecf
Nico Weidmann <nicoweidmann@Nicos-MBP.fritz.box>
2017-03-01 22:11:51 +0100

    Removed dependency on hamcrest matcher
    removed a dependency on a hamcrest matcher that I created myself in an
    earlier commit by using @FlorianPfisterer s TestUtil class.

--- Final2-Another Candy Crush Clone/edu/kit/informatik/matchthree/tests/MoveRotateSquareClockwiseMoveTest.java
+++ Final2-Another Candy Crush Clone/edu/kit/informatik/matchthree/tests/MoveRotateSquareClockwiseMoveTest.java
@@ -1,8 +1,9 @@
 package edu.kit.informatik.matchthree.tests;
 
 import static org.junit.Assert.*;
-// requires hamcrest matchers in build path
-import static org.hamcrest.Matchers.*;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -78,17 +79,18 @@ public class MoveRotateSquareClockwiseMoveTest {
         Board board = new MatchThreeBoard(Token.set("*AXY="), "*AX;Y**;X*=");
         Position current = Position.at(1, 0);
         Move rotateCW = new MoveFactoryImplementation().rotateSquareClockwise(current);
-        assertThat(rotateCW.getAffectedPositions(board),
-                containsInAnyOrder(current, current.plus(1, 0), current.plus(1, 1), current.plus(0, 1)));
+        TestUtils.assertSetEquals(rotateCW.getAffectedPositions(board), Stream
+                .of(current, current.plus(1, 0), current.plus(1, 1), current.plus(0, 1)).collect(Collectors.toSet()));
     }
+
     @Test
     public void affectedPositions2() {
 
         Board board = new MatchThreeBoard(Token.set("*AXY="), "*AX;Y**;X*=");
         Position current = Position.at(1, 0);
         Move rotateCCW = new MoveFactoryImplementation().rotateSquareClockwise(current).reverse();
-        assertThat(rotateCCW.getAffectedPositions(board),
-                containsInAnyOrder(current, current.plus(1, 0), current.plus(1, 1), current.plus(0, 1)));
+        TestUtils.assertSetEquals(rotateCCW.getAffectedPositions(board), Stream
+                .of(current, current.plus(1, 0), current.plus(1, 1), current.plus(0, 1)).collect(Collectors.toSet()));
     }
 
 }
