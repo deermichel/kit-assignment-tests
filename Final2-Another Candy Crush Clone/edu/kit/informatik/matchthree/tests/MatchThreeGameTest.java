f48706009d0ab270e73bd580fb4b32334a0d86ff
luk3b <lukas.brocke@gmail.com>
2017-03-19 16:34:39 +0100

    The MatchThreeGame is _not_ supposed to set the FillingStrategy

--- Final2-Another Candy Crush Clone/edu/kit/informatik/matchthree/tests/MatchThreeGameTest.java
+++ Final2-Another Candy Crush Clone/edu/kit/informatik/matchthree/tests/MatchThreeGameTest.java
@@ -12,6 +12,7 @@ import edu.kit.informatik.matchthree.MaximumDeltaMatcher;
 import edu.kit.informatik.matchthree.MoveFactoryImplementation;
 import edu.kit.informatik.matchthree.framework.Delta;
 import edu.kit.informatik.matchthree.framework.Position;
+import edu.kit.informatik.matchthree.framework.RandomStrategy;
 import edu.kit.informatik.matchthree.framework.Token;
 import edu.kit.informatik.matchthree.framework.exceptions.BoardDimensionException;
 import edu.kit.informatik.matchthree.framework.interfaces.Board;
@@ -25,6 +26,9 @@ import edu.kit.informatik.matchthree.framework.interfaces.Move;
  * @author Luke Brocke
  */
 public class MatchThreeGameTest {
+    // A FillingStrategy will be set for the board, the MatchThreeGame constructor MUST NOT do that
+    // see https://ilias.studium.kit.edu/ilias.php?ref_id=583580&cmdClass=ilobjforumgui&thr_pk=85450&cmd=viewThread&cmdNode=75:r6&baseClass=ilrepositorygui
+    
     /**
      * {@link Game#acceptMove(Move)} should throw a {@link BoardDimensionException} if the given
      *   {@link Move} cannot be applied
@@ -32,6 +36,7 @@ public class MatchThreeGameTest {
     @Test (expected = BoardDimensionException.class)
     public void moveAcceptExceptionTest() {
         Board board = new MatchThreeBoard(Token.set("AB"), 2, 2);
+        board.setFillingStrategy(new RandomStrategy());
         
         MoveFactoryImplementation factory = new MoveFactoryImplementation();
         Move flipRight = factory.flipRight(Position.at(1, 0));
@@ -48,6 +53,7 @@ public class MatchThreeGameTest {
     @Test
     public void moveAcceptTest() {
         Board board = new MatchThreeBoard(Token.set("AB"), 2, 2);
+        board.setFillingStrategy(new RandomStrategy());
         
         MoveFactoryImplementation factory = new MoveFactoryImplementation();
         Move flipRight = factory.flipRight(Position.at(0, 0));
@@ -64,6 +70,7 @@ public class MatchThreeGameTest {
     @Test
     public void initializeScoreTest() {
         Board board = new MatchThreeBoard(Token.set("AB"), 5, 5);
+        board.setFillingStrategy(new RandomStrategy());
         Matcher matcher = new MaximumDeltaMatcher(new HashSet<>(Arrays.asList(Delta.dxy(0, 1))));
         
         MatchThreeGame game = new MatchThreeGame(board, matcher);
@@ -78,6 +85,7 @@ public class MatchThreeGameTest {
     @Test
     public void initializeFillBoardTest() {
         Board board = new MatchThreeBoard(Token.set("AB"), 5, 5);
+        board.setFillingStrategy(new RandomStrategy());
         Matcher matcher = new MaximumDeltaMatcher(new HashSet<>(Arrays.asList(Delta.dxy(0, 1))));
         
         MatchThreeGame game = new MatchThreeGame(board, matcher);
