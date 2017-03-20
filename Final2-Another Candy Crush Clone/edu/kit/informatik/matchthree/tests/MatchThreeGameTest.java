1320e4a1ef478fd86eb6b7223eaa2751b2575821
Nico Weidmann <nicoweidmann@web.de>
2017-03-20 18:06:44 +0100

    Removing weird merge conflict

--- Final2-Another Candy Crush Clone/edu/kit/informatik/matchthree/tests/MatchThreeGameTest.java
+++ Final2-Another Candy Crush Clone/edu/kit/informatik/matchthree/tests/MatchThreeGameTest.java
@@ -4,10 +4,6 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.HashSet;
-<<<<<<< HEAD
-
-=======
->>>>>>> DeerMichel/master
 import org.junit.Ignore;
 import org.junit.Test;
 
